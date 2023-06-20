ecf85ca47bbd3c7caee930b360aaaf76f03046bc
Yusuf <yusuf2002kh@gmail.com>
2023-06-20 22:44:33 +0300

    firs

---
+++ src/components/Hero.jsx
@@ -0,0 +1,95 @@
+import {
+  Box,
+  Flex,
+  Image,
+  Text,
+  Container,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
+
+const Hero = () => {
+  return (
+    <Box
+      w={"full"}
+      h={"600px"}
+      backgroundImage={"url(../../images/bg.png)"}
+      backgroundRepeat={"no-repeat"}
+      backgroundSize={"cover"}
+    >
+      <Container maxW={"container.xl"}>
+        <Box pt={"100px"}>
+          <Flex justify={"space-between"} align={"center"} w={"500px"}>
+            <Text
+              p={2}
+              bg={"#fff"}
+              opacity={".7"}
+              fontWeight={"300"}
+              fontSize={"24px"}
+              lineHeight={"110%"}
+              letterSpacing={".03em"}
+              textTransform={"uppercase"}
+              color={"#03030B;"}
+            >
+              20-21-22-iyun
+            </Text>
+
+            <Flex justify={"space-between"} align={"center"} gap={2}>
+              <Flex align={"center"} gap={1}>
+                <Text
+                  fontWeight={"300"}
+                  fontSize={"24px"}
+                  lineHeight={"110%"}
+                  textAlign={"right"}
+                  letterSpacing={"0.03em"}
+                  textTransform={"uppercase"}
+                  color={"#5B6375"}
+                >
+                  20:00
+                </Text>
+                <Image src="../../images/fi_clock.png" />
+              </Flex>
+              <Button bg={"#EF3833"} borderRadius={"20px"} color={"#fff"}>
+                ONLINE
+              </Button>
+            </Flex>
+          </Flex>
+          <Heading
+            fontWeight={"700"}
+            fontSize={"80px"}
+            letterSpacing={".02em"}
+            color={"#fff"}
+          >
+            Grafik Dizayn
+          </Heading>
+          <Heading
+            fontWeight={"300"}
+            fontSize={"40px"}
+            letterSpacing={"0.3em"}
+            textTransform={"uppercase"}
+            color={"#FFFFFF"}
+          >
+            Sohasiga Kirish
+          </Heading>
+          <Flex mt={5} gap={3} align={"center"}>
+            <Image src="../../images/vector.png" />
+            <Box>
+              <Text
+                fontWeight={"3000"}
+                fontSize={"24px"}
+                lineHeight={"110%"}
+                letterSpacing={"0.03em"}
+                color={"#FFFFFF"}
+              >
+                Ismoil Mahmudjonovdan <br />
+                <span style={{ fontWeight: "bold" }}> Bepul Master-Klass</span>
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Hero;
